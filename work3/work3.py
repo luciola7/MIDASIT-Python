e979d30f246afa34e7e6bf8ef4cfa9431d25e107
kimbusung <superbsung@gmail.com>
2016-04-28 20:22:38 +0900

    괄호 하나가 빠져있었어요

--- work3/work3.py
+++ work3/work3.py
@@ -56,7 +56,7 @@ def test(got, expected):
     prefix = ' OK '
   else:
     prefix = '  X '
-  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+  print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
 
 
 # Calls the above functions with interesting inputs.
